b84dc0e8db9341d35d56271c0df8cce2aae242f3
Durwasa Chakraborty <durwasa.chakraborty@gmail.com>
2015-02-11 01:06:17 +0530

    com

---
+++ TSORT.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+int compare(void const *, void const*);
+int main(int argc, char const *argv[])
+{
+    int i=0,n,p=0;
+    scanf("%d",&n);
+    int a[n];    
+    while(n--)
+    {
+        scanf("%d",&a[i]);
+        i++;
+    }
+    qsort(a,i,sizeof(int),compare);
+    while(i--)
+    {
+        printf("%d\n",a[p]);
+        p++;
+    }     
+    return 0;
+}
+
+int compare(void const *p,void const *q)
+{
+    return (*(int*)p-*(int*)q);
+}
